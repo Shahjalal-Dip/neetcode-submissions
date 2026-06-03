78c568c3c92ce283f753dcfe25750288de0012ba
Shah Jalal Dip <shahjalaldip246@gmail.com>
2026-06-03 17:13:36 +0600

    Add: sql-customers-bought-a-b-not-c - submission-0

---
+++ SQL/sql-customers-bought-a-b-not-c/submission-0.sql
@@ -0,0 +1,15 @@
+SELECT c.customer_id, c.customer_name
+From customers c
+INNER JOIN orders o ON o.customer_id = c.customer_id
+Group BY c.customer_id, c.customer_name
+Having SUM(CASE When product_name = 'A' 
+                   THEN 1 
+                   ELSE 0 END) > 0 AND 
+       SUM(CASE WHEN product_name = 'B' 
+                   THEN 1 
+                   ELSE 0 END) > 0 AND
+       SUM(CASE WHEN product_name = 'C' 
+                   THEN 1 
+                   ELSE 0 END) = 0
+ORDER BY customer_name;
+
